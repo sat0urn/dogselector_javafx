042a8c994771853a80bec87d000384684da5006e
sat0urn <nissanfordo03@gmail.com>
2022-10-29 12:24:21 +0600

    new&updated

--- src/main/java/com/example/DBConfig.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class DBConfig {
-    public final static String URL = "jdbc:postgresql://localhost:5432/dogacquisition";
-    public final static String NAME = "postgres";
-    public final static String PASSWORD = "1234";
-}
